51bcaa192288863fbf6cb542ebe5f31656970609
rponciano <romulo_37@hotmail.com>
2018-11-03 21:23:15 -0300

    Add pixels replace script

---
+++ replace-image-pixels.py
@@ -0,0 +1,23 @@
+# Rômulo Ponciano
+# 03 Nov 2018
+# This script replace all the pixels of a defined color, in an image, for another defined color. E.g. If you want to replace all red pixels with white pixels.
+
+from PIL import Image 
+
+imgPathWithFormat = ''
+rgb = (0, 0, 0)
+finalColor = (255, 255, 255)
+imgOutputPathWithFormat = ''
+
+im = Image.open(imgPathWithFormat)
+pixels = list(im.getdata())
+
+idx = 0
+for i in pixels:
+	if i == rgb:
+		pixels[idx] = (255,255,255)
+	idx = idx + 1
+
+im2 = Image.new(im.mode, im.size)
+im2.putdata(pixels)
+im2.save(imgOutputPathWithFormat)
